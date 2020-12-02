e96e997f59123de47a2a57b7402d79ad4932e15f
startrekdude <cheesedude54321@gmail.com>
2020-12-02 02:23:30 -0500

    Add song

---
+++ _gen/Cul de Sac.py
@@ -0,0 +1,22 @@
+*d,s=""".8 C$~6!N(~; D/~' &"~uoY2 >/~ot& A$~I tub ,uoy devol I esuaceb , ?0~+ R,~klaw oT
+enobkcab eht pu krow yllanif' U,~I nehw& K1~t'nod( ["~7 ,$~.tseuq noisiv elttob-llip ,tihsllub emos nO
+& E*~lla er'uoY .t'no&  #~tub ...7 6!~shgual tug dna slegna wons fO
+skcabhsalf evah d'I( J.~lleW8 0!~.uoy yrrac' F)~tsuj I
+.tog smra ym kaew woh& 9-~d'uoY
+,emoh em deklaw& T&~I
+
+. A*~P#U&~sensselkcer doohyob tsuJ
+htiw tcatnoc ni em& Z#~gnihtyreve pu dekcuf eW .cirotehr hsarc-rac s'tI
+.tfilpohs ot desu( 5'~ew erehw noitats sag eht fo edistuO
+daeh ym edisni uoy f& %'~mi tog& ?%~
+.enola eb reven d'ew* H ~ .elcnu uoy llac dluow sdik ym thguoht I
+.elbuort& @!~o peek t'nac ew oN1 T!~.lwarps& B ~ sselerac + ?!~tercnoc nabrubus dloc fo tuO
+yvi nosiop ekil no gnidloh* S#~ .og gnittel m'I
+
+.siht morf emoc nac doog oN .kcis em sekam ecnelis eht dnA
+ssih epat revo snoissergsnart ym gnissefnoc* Q!~
+.gninetsil s'ydobon wonk I os ,ytpme syawla s'taht esuoh a nI
+enihcam gnirewsna na no segassem gnivael neeb ev'I"""
+z="int(f'%s',2)"%(2*'{ord(d.pop())-32:06b}')
+while d:*d,c=d;s+=eval('c#'*(c<'~')+f's[-{z}:][:{z}]')
+print(s)
