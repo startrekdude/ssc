76b71262ee37e64e64792fb940d4fc291083d97e
startrekdude <cheesedude54321@gmail.com>
2021-03-11 02:47:48 -0500

    add song

---
+++ _gen/Ride Forever.py
@@ -0,0 +1,26 @@
+*d,s=""".#"F3~$"$"~.<">/~ssorca raet- L.~knits& )3~nu, 1.~w ,srups& I'~pu kciK
+esroh& !4~ti teg ,elddas dlo& 8&~ffo tsud ot emit s'tI
+taorht & 0!~& +'~ctarcs& -+~dnA
+yzarc enog s'dlro& 1%~knih& 7-~fI
+ekorb teg sevlesruoy tel t'nod ,nem dlo) *'~ot& 1'~ oS>"^(~htaenrednU
+llat gnidnats m' U-~lla ti tog I ,ylf& U%~o noillats* D ~rednu& 8%~swolb taht dniw eht ekiL
+seid reven ti efil ruoy ,seiromem) 4 ~' V+~dirp ym evah' 6 ~I dna& Y ~aerb llits ma I
+, 1 ~ on ,enog' 7!~I
+gnorw gnihtemos tog ev'uoy esuac' stius gib uoy lla kcab pmuj yas I
+dniheb raf t'nia esuoh& [(~,esnecil s'revird ym koot ' G#~ilb ma * = ~& L#~nam dlo na m'I em llet yehT
+
+snier eht ni llup thgim eW
+neht ylno s'ti llew ,llac slegna eht dluohS
+egac a ni nemesroh peek t'nac uoY
+reverof edir annog er'eW
+
+seiks atreblA eulb eseht morf gnillaf' = ~T
+dnuorg& I$~revo lla er'yeht ,sraet uoy wohs ll'I ,lleH
+?yrc I fi em ksa uoy -- remmus tsal deid ycuL ym dnA
+drawer naeroK ym ,ligriV nos ym tsol' E#~sraW dlroW ow, ."~tub ,drawoc a dellac neeb ev'& 6#~
+duolc rednuht a s'taht -- niar ekil sleef ti ,yeH
+egnahc dna pilf dna dneb ,em tuoba dlrow siht nees ev'& Z ~etaG s'liveD& = ~hcnar a no ,efil ym lla raen edor I dnA
+8981 ,evalS taerG fo htron pu nrob saw I lleW"""
+z="int(f'%s',2)"%(2*'{ord(d.pop())-32:06b}')
+while d:*d,c=d;s+=eval('c#'*(c<'~')+f's[-{z}:][:{z}]')
+print(s)
