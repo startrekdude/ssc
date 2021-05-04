904ccb3462053a6d44d45f4e8e3586c2874e2fcf
startrekdude <cheesedude54321@gmail.com>
2021-05-04 08:56:35 -0400

    add song

---
+++ _gen/Devil Town v2.py
@@ -0,0 +1,32 @@
+*d,s="""oE$X3~retaw( 53~sehcnarb smrof doD S"~duol oot lla& <$~htuo& I1~disni egnahc dna edaf sruoloc> Q"~E
+?duorp) ;-~uoy od woH
+
+revir eht ssorca smiws mossolB
+sregnif ym otni hteet ym knis I
+nwod llaf sduolc ehT
+won ,remmid* #-~steg enihsnus ehT
+seye( -.~c I emit yreve tuB
+emit-gnineve( ^-~ylevol s'tIH$A)~uoy, T&~rom er'yeht( N#~eb meht evael tsuJ
+seohs etiruovaf ruoy ni sredipS
+gnirebmemer htrow* V)~( &)~kniht I' 2)~eman ym togrof I
+
+nworb dna neerg si sruoy ,yepirts dnA
+etihw s'enim ,snwog gnisserd ruo ni* .!~( *)~eracs) *)~gnihton esuac'- O%~/ ")~daed lla' ?'~
+oot yaw siht nellaf ev'uoy wonk ' N ~dab os& V ~t'nseod gnillaF
+uoy htiw m'I nehw refa+ L ~leef I tuB
+wen+ O$~fo deracs elttil a teg llits I
+thgin* Y ~i ekam ll'ew ,thg& 3 ~h ym dloH
+semit dnasuoht rehtona* @"~dnim ym esol ll'I
+emitremmus eht ni redloc si ) *$~D
+
+syadhtrib owt rof elttes ll'I& ."~& R ~T
+evol ni t'nera yddad dna muM
+yas yeht tahw s'taht tsael ta ,dam s'ehS
+niaga bmud gnihtemos dias uoY
+> %!~enif er'eW
+won rac wen a thguob sah daD
+won su hctac annog s'eno on ,thgir ,haeY
+nwot lived ni thgirla s'efiL"""
+z="int(f'%s',2)"%(2*'{ord(d.pop())-32:06b}')
+while d:*d,c=d;s+=eval('c#'*(c<'~')+f's[-{z}:][:{z}]')
+print(s)
