7fb08be6902f9e7143eb3f5a547a352bcb88fb8f
startrekdude <cheesedude54321@gmail.com>
2021-12-12 01:37:52 -0500

    ye

---
+++ _gen/Vain Glory Opera.py
@@ -0,0 +1,19 @@
+*d,s="""e"#H$~%#V-~#$Q*~$$O&~?) ?%~loof+ '"~yalp a ta oreh a, ;!~r t'nac ew fi llaf a( ;!~thgirb s'ti kniht dluow eW
+?yad+ E ~thgin eht( @ ~?esidarap a tuohtiw lleh s'tahW
+
+B!B!~ediug dekciw ym eb yrolg niaV
+loof a ekirts ot edirp& 8 ~og eW
+mood fo dnah eht thgif ot evil eW
+
+seil ylno toN
+esiugsid ni luos A
+seye s'ymene& J ~iheb tuB
+
+yrotsih eb lliw yehT
+edirp ym dna hpmuirt yM
+ekorts eno ta neveS
+yrotciv a em gnirb ot yaw ym nioj y( ; ~edis ym no eb sdog eht yaM
+nekops neeb evah sreyarp ehT"""
+z="int(f'%s',2)"%(2*'{ord(d.pop())-32:06b}')
+while d:*d,c=d;s+=eval('c#'*(c<'~')+f's[-{z}:][:{z}]')
+print(s)
---
+++ _gen/Wake Up, Get Up, Get Out There.py
@@ -0,0 +1,38 @@
+*d,s="""eP##-~C 0)~sirbed( Q)~fles& '+~niF
+eerf eb dna ffo ksam & 0&~aT
+haey ,tuo gnir s+ G*~teL? 2)~yats tonnac uoy evil d'' [&~yaw' ;%~naht& 8&~ot erom s'erehT> ))~2 H!~er on* E!~sllot lleb& ="~a dnA
+?& %*~noillim a llik d'. K!~& Q(~si ohW
+
+?su rof noissim a & O&~sI
+?taht ,emirc a ti t'niA
+?su naht retteb s'eh skniht ohW
+?drol hgih eht s'ohW
+
+deerb laiceps emos er'yeht ekiL
+& )'~tser eht llA
+no nwod kool yehT
+rae& 3 ~t gnidih secaf riehT
+traeh ta kcis lla erA* 9&~T
+
+( ^!~'now efil no dloh uoy fI
+gni' ;#~uohtiw tsaf evom t'naC
+gnikam eht ni seil gnittawS+ U!~er'uoy enigami tsuJ
+
+?egnahc tnaw ydobon seod yhW
+erif ekil regna& > ~deeF
+srail tsniaga eciov ruoy esiaR
+ereht tuo ( ( ~teg ,pu ekaW
+' W ~attuo & N!~t'nod ew fI
+ecnereffid a ekam ew naC
+?ereh er'ew taht naem ti dluoc tahW
+sselesu s'ti ,hO
+
+lla su fo srekaerb ehT
+seil htiw su gnidnilB
+srekaf eht ,kooL
+lla ta ereh ton m'I ebyaM
+?euqinu ton I mA
+?I ma ohW"""
+z="int(f'%s',2)"%(2*'{ord(d.pop())-32:06b}')
+while d:*d,c=d;s+=eval('c#'*(c<'~')+f's[-{z}:][:{z}]')
+print(s)
