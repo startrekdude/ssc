9e71a1bf1c7a5b4ff387d19c9dd62cf92c15aeca
startrekdude <cheesedude54321@gmail.com>
2022-11-29 06:12:32 -0500

    add a song

---
+++ _gen/Star Sky.py
@@ -0,0 +1,28 @@
+*d,s="""lL$M(~a no reveroF
+syats rebmemer straeh ruo tahW
+) /'~ nac&  *~fo llA* S(~& 4$~* R(~( 1 ~0 R(~o= R(~morF
+thgirb erif( X)~peeK. H)~r, R(~smaerd erutuf fo yromeM
+llac eht leef I
+ega ot egA
+
+> 9"~5 5"~sdrow& J%~ni ecilos dnif+ T"~vig uoy& L ~hcuo& -"~& N#~7 +"~ta& *"~T
+
+os luos& %$~sserac rO
+evol& Q#~reviled+ "!~erf em tes os devol eb* A ~peels fo& (#~eht esare tonnac I
+em rof egap eht nruB
+
+yalp ew gno& Q#~,elbappotsnU
+ecaps renni ni edilloc sdlroW
+ecarbme won tneser& &!~tsaP
+ecarg& W ~vol ruoY
+ecaf- 1 ~eman ruoy wenk I
+
+ecalp dna emit rehtona ni tiL
+erif fo semalf niwT
+thgil fo srorriM ,I dna uoY
+nus htiw thgin( 1 ~tniaP
+yks eht gnidiR
+era ew ereH"""
+z="int(f'%s',2)"%(2*'{ord(d.pop())-32:06b}')
+while d:*d,c=d;s+=eval('c#'*(c<'~')+f's[-{z}:][:{z}]')
+print(s)
